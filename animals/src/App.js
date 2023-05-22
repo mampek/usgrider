ccfe01e67225f4fbfa8c7fecc42433377706f185
Krystian Mampe <mampek@gmail.com>
2023-05-22 15:34:25 +0100

    Complete application for practising state and events

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,4 +1,25 @@
+import "./App.css";
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
-  return <div>Show animal list here</div>;
+  const [animals, setAnimals] = useState([]);
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+  return (
+    <div className="app">
+      <button onClick={handleClick}>Add Animal</button>
+      <div className="animal-list">{renderedAnimals}</div>
+    </div>
+  );
 }
 export default App;
