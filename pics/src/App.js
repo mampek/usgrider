4690d469061cb2060dc6909e7f95ab3f6e157086
Krystian Mampe <mampek@gmail.com>
2023-05-25 12:38:26 +0100

    Working with forms and input elements

--- pics/src/App.js
+++ pics/src/App.js
@@ -1,12 +1,15 @@
 import SearchBar from "./components/SearchBar.js";
+
 function App() {
   const handleSubmit = (term) => {
     console.log("Do a search with", term);
   };
+
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
     </div>
   );
 }
+
 export default App;
